6670096650f47e10bbee09f5bee0f593950b6561
sliptype <sliptype@gmail.com>
2018-12-28 14:52:31 -0600

    Break out arguments into helper functions

--- src/SnabbdomHelpers.purs
+++ src/SnabbdomHelpers.purs
@@ -2,16 +2,11 @@ module SnabbdomHelpers where
 
 import Prelude
 
+import Effect (Effect)
 import Data.Maybe (Maybe(..))
 import Foreign.Object (Object, empty)
-import Snabbdom (VNodeData, VNodeEventObject, VNodeHookObjectProxy, VNodeProxy(..), patch, toVNode, toVNodeEventObject, toVNodeHookObjectProxy)
+import Snabbdom (VNodeData, VNodeProxy(..), toVNodeEventObject, toVNodeHookObjectProxy)
 import Snabbdom (h) as Snabbdom
-
-data HArgument
-  = Selector String
-  | Children (Array VNodeProxy)
-
-type HArguments = Array HArgument
 
 emptyVNodeData :: VNodeData
 emptyVNodeData =
@@ -20,8 +15,25 @@ emptyVNodeData =
   , hook : toVNodeHookObjectProxy { insert : Nothing, update : Nothing, destroy : Nothing }
   }
 
-h :: HArguments -> VNodeProxy
-h [Selector s] = Snabbdom.h s emptyVNodeData []
-h [Children c] = Snabbdom.h "test" emptyVNodeData c
-h _ = Snabbdom.h "test" emptyVNodeData []
+h :: VNodeProxy
+h = Snabbdom.h "" emptyVNodeData []
 
+sel :: String -> VNodeProxy -> VNodeProxy
+sel s (VNodeProxy v) = VNodeProxy $ v { sel = s }
+
+-- TODO: Use a record for props
+props :: Object String -> VNodeProxy -> VNodeProxy
+props p (VNodeProxy v) = VNodeProxy $ v { data { attrs = p } }
+
+on :: forall a. Object (a -> Effect Unit) -> VNodeProxy -> VNodeProxy
+on e (VNodeProxy v) = VNodeProxy $ v { data { on = toVNodeEventObject e } }
+
+-- TODO: Because this isn't using h, the text only children aren't being converted
+-- to VNodes
+children :: Array VNodeProxy -> VNodeProxy -> VNodeProxy
+children c (VNodeProxy v) = VNodeProxy $ v { children = c }
+
+-- h # sel "#div"
+--   # props { disabled: "true" }
+--   # on { click: doSomething }
+--   # children []
